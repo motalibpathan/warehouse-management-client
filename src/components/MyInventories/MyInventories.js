389ea82417ad9a9307f645f48cc0b69e1bf90dc7
motalibpathan11 <73628059+motalibpathan11@users.noreply.github.com>
2022-05-08 01:23:34 +0600

    varifiying jwt when go to my inventories page

--- src/components/MyInventories/MyInventories.js
+++ src/components/MyInventories/MyInventories.js
@@ -1,5 +1,7 @@
+import { signOut } from "firebase/auth";
 import React, { useEffect, useState } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
+import { useNavigate } from "react-router-dom";
 import auth from "../../firebase.init";
 import DataLoading from "../DataLoading/DataLoading";
 import Manage from "../Manage/Manage";
@@ -8,17 +10,33 @@ const MyInventories = () => {
   const [inventories, setInventories] = useState([]);
   const [dataLoading, setDataLoading] = useState(false);
   const [user] = useAuthState(auth);
+  const navigate = useNavigate();
 
   useEffect(() => {
     setDataLoading(true);
     const url = `http://localhost:5000/inventoryByEmail?email=${user.email}`;
-    fetch(url)
-      .then((res) => res.json())
+    fetch(url, {
+      headers: {
+        authorization: `Bearer ${localStorage.getItem("access-token")}`,
+      },
+    })
+      .then((res) => {
+        console.log(res.status);
+        if (res.status === 401 || res.status === 403) {
+          signOut(auth);
+          navigate("/login");
+        } else {
+          return res.json();
+        }
+      })
       .then((data) => {
         setInventories(data);
         setDataLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
       });
-  }, [user]);
+  }, [user, navigate]);
 
   return (
     <>
